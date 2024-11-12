6243832cffcbb144b50bb957ac0e433f9bfc5cf5
Pooventhan <pooventhan@airasia.com>
2024-11-12 23:03:49 +0800

    Log request payload.

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -10,6 +10,7 @@ router.post('/', async (req: Request, res: Response) => {
     res.status(201).json();
   } catch (error) {
     console.error('Error saving payload:', error);
+    console.error(`Payload=${JSON.stringify(req.body)}`);
     res.status(500).json({ error: 'Failed to save payload' });
   }
 });
